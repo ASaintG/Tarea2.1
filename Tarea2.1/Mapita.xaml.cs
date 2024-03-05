6f9c195a582c49049bf294cff4bb2eae94e1691a
Ivan Barahona <igyt2015@gmail.com>
2024-03-04 19:27:14 -0600

    Fin

--- Tarea2.1/Mapita.xaml.cs
+++ Tarea2.1/Mapita.xaml.cs
@@ -1,33 +1,27 @@
 using Microsoft.Maui.Controls.Maps;
 using Microsoft.Maui.Maps;
-using System.Text;
 using Tarea2._1.Models;
 
 namespace Tarea2._1;
 
 public partial class Mapita : ContentPage
 {
-    public Mapita(double latitude, double longitude, Countrys country)
-    {
-        InitializeComponent();
+	public Mapita(double latitude, double longitude, Countrys country)
+	{
+		InitializeComponent();
 
         var pin = new Pin
         {
-            Label = country.name,
-            Location = new Location(latitude, longitude),
+            
+            Label = $"País: {country.name} Población: {country.pobla}",
+            //$" Área territorial: {country.area} km²",            
+            Location = new Location(latitude, longitude), // Usar Position en lugar de Location
             Type = PinType.Place
         };
 
-        // Construir la descripción del punto
-        StringBuilder description = new StringBuilder();
-        description.AppendLine($"País: {country.name}");
-        description.AppendLine($"Población: {country.pobla}");
-        description.AppendLine($"Área territorial: {country.area} km²");
-
-        // Asignar la descripción al pin
-        pin.Label = description.ToString();
-
+        // Agregar el pin al mapa
         mapa.Pins.Add(pin);
+        // Centrar el mapa en la ubicación del pin
         mapa.MoveToRegion(MapSpan.FromCenterAndRadius(pin.Location, Distance.FromKilometers(100)));
     }
 }
